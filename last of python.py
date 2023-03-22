a1831159cd04c155595d0e53c09836c91cceb554
smithi2008 <103350855+smithi2008@users.noreply.github.com>
2023-03-22 19:38:22 +0700

    Add files via upload

---
+++ last of python.py
@@ -0,0 +1,27 @@
+import pyautogui
+import webbrowser
+url = 'https://pwa.zoom.us/wc/'
+webbrowser.open(url)
+pyautogui.click(962,383,duration=2)
+
+pyautogui.write('4486965231',interval=0.5)
+
+pyautogui.press('enter')
+
+pyautogui.click(1049,281,duration=2)
+
+pyautogui.write('4Qx2ZC',interval=0.5)
+
+pyautogui.press('enter')
+
+pyautogui.click(956,510,duration=2)
+
+pyautogui.click(946,779,duration=2)
+
+pyautogui.click(925,1009,duration=15)
+
+pyautogui.click(1670,970,duration=2)
+
+pyautogui.write("Im here!!!",interval=0.5)
+
+pyautogui.press('enter')
